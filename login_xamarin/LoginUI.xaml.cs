bb3aea49f4cfdba24b597d60bc11f2d6b866978a
JOBINTJ24 <jobintj2023b@mca.ajce.in>
2023-07-12 08:55:05 +0530

    facebook clone

---
+++ login_xamarin/LoginUI.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace login_xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "admin" && txtPassword.Text == "123")
+            {
+                Navigation.PushAsync(new home());
+            }
+            else
+            {
+                DisplayAlert("Ops..", "username or password is incorrect!", "ok");
+            }
+
+        }
+       
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registerpage());
+
+        }
+    }
+}
---
+++ login_xamarin/Registerpage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace login_xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registerpage : ContentPage
+    {
+        public Registerpage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string name = txtname.Text;
+            string email = txtEmail.Text;
+            string phone = txtPhone.Text;
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string confirmPassword = txtConform.Text;
+
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(phone) ||
+                string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(confirmPassword))
+            {
+                // Display an error message or handle the validation failure
+                DisplayAlert("Error", "Please fill in all fields", "OK");
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                // Display an error message or handle the validation failure
+                DisplayAlert("Error", "Password and Confirm Password must match", "OK");
+                return;
+            }
+            Navigation.PushAsync(new LoginUI());
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginUI());
+
+        }
+    }
+}
